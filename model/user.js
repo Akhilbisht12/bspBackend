7097500422931b0ac1593dc36d49de3ec6a8d2c3
Akhilbisht12 <akhilbisht83@gmail.com>
2022-01-10 22:52:50 +0530

    seperate directory

---
+++ model/user.js
@@ -0,0 +1,74 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+
+  email: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: Number,
+    required: true,
+  },
+  password: {
+    type: String,
+  },
+
+  isverified: {
+    type: Boolean,
+    required: true,
+  },
+  resetVerified: {
+    type: Boolean,
+    required: false,
+  },
+
+  courses: [
+    {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: "Course",
+    },
+  ],
+
+  preferences: [{ type: String }],
+
+  Bookmark: [
+    {
+      type: Schema.Types.ObjectId,
+      required: false,
+      ref: "Course",
+    },
+  ],
+
+  swot: [
+    {
+      village: String,
+      strengths: String,
+      weaknesses: String,
+      opportunities: String,
+      threats: String,
+      date: Date,
+    },
+  ],
+  influencers: [
+    {
+      name: String,
+      phone: Number,
+      occupation: String,
+      district: String,
+      mandal: String,
+      village: String,
+    },
+  ],
+  //Token:String,
+  //resetToken:String,
+  //resetTokenExpiration:Date,
+});
+
+module.exports = mongoose.model("User", userSchema);
